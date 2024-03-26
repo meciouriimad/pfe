5bd57673c703cec04b1bf77881a18aa34bd37dc7
imadmeciouri <imadmeciouri32@gmail.com>
2024-03-26 02:10:32 +0100

    change style

--- recipedetails.js
+++ recipedetails.js
@@ -8,16 +8,15 @@ const RecipeDetails = ({ particularRecipe, handleClose }) => {
     <Modal.Header closeButton>
       <Modal.Title>{particularRecipe.recipename}</Modal.Title>
     </Modal.Header>
-    <Modal.Body>
+    <Modal.Body>                  
       <p>Description: {particularRecipe.description}</p>
       <p>Instructions: {particularRecipe.instructions}</p>
       <p>Ratings: {particularRecipe.ratings}/10</p>
       <p>Cook Time: {particularRecipe.cooktime} h</p>
       <p>Preparation Time: {particularRecipe.preparationtime} h</p>
-
       <h4>Ingredients:</h4>
       <ul>
-        {particularRecipe.ingredients.map((ingredient, index) => (
+        {particularRecipe.ingredient.map((ingredient, index) => (
           <li key={index}>
             <a href={ingredient.link}>{ingredient.quantity} {ingredient.unit} {ingredient.ingredient_name} </a>
           </li>
